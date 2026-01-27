6ae92a83bc88046d0668577db8abfe7dad84175e
Nil <Xwszc7@gmail.com>
2026-01-27 22:06:36 +0800

    Initial commit

---
+++ Assets/Plugins/Text Animator Extras/Typewriter Sound/TAnimSoundWriter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Febucci.UI.Examples
+{
+    /// <summary>
+    /// Extra example class for the TextAnimator plugin, used to add sounds to the TextAnimatorPlayer.
+    /// </summary>
+    [AddComponentMenu("Febucci/TextAnimator/SoundWriter")]
+    [RequireComponent(typeof(Core.TypewriterCore))]
+    public class TAnimSoundWriter : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Assert.IsNotNull(GetComponent<Core.TypewriterCore>(), "TAnimSoundWriter: Component TAnimPlayerBase is not present");
+
+            GetComponent<Core.TypewriterCore>()?.onCharacterVisible.AddListener(OnCharacter);
+        }
+
+        void OnCharacter(char character)
+        {
+            // Play Sound On Character Shown
+
+        }
+    }
+}
