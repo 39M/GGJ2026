8b5f0811dfd8b879e3cf7b67c741f6d882eb487d
苏言 <135815453+zengrui01@users.noreply.github.com>
2026-01-31 15:15:11 +0800

    老虎吃了羊，老虎会眩晕一下，羊会弹开反向

--- Assets/Scripts/GameCfg.cs
+++ Assets/Scripts/GameCfg.cs
@@ -37,6 +37,14 @@ namespace GGJ
         public Coin CoinPrefab;
         [LabelText("发射面具速度")]
         public float BulletSpeed;
+        [Header("吃掉玩家")]
+        [LabelText("偷分比例(0~1，从被吃者扣取并加给吃者)")]
+        [Range(0f, 1f)]
+        public float EatStealRatio = 0.5f;
+        [LabelText("吃人者眩晕时间(秒)")]
+        public float EatStunDuration = 1.5f;
+        [LabelText("被吃者弹开距离(格，防止短时间反复触发)")]
+        public float EatPushDistance = 2f;
         public Dictionary<MaskType, MaskCfg> MaskDefine;
         
         public MaskCfg GetMaskCfg(MaskType type) => MaskDefine[type];
